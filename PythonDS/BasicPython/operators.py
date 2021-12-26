7487a1cecee093641d1703af09f55829fbe64a00
James Ortiz <jamesaortiz75@gmail.com>
2021-12-25 23:44:48 -0500

    Operators in Python

---
+++ PythonDS/BasicPython/operators.py
@@ -0,0 +1,17 @@
+#
+# File: operators.py
+# Author: James Anthony Ortiz
+# Description: Operators in python
+#
+
+print(2+3*4) #14
+print((2+3)*4) #20
+print(2**10) #1024
+print(6/3) #2.0
+print(7/3) #2.33333333333
+print(7//3) #2
+print(7%3) #1
+print(3/6) #0.5
+print(3//6) #0
+print(3%6) #3
+print(2**100) # 1267650600228229401496703205376
