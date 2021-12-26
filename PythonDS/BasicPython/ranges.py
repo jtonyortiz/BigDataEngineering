e4f6d949dc8f78b9279745bc0e8d4a22933636eb
James Ortiz <jamesaortiz75@gmail.com>
2021-12-25 23:45:41 -0500

    Ranges in python

---
+++ PythonDS/BasicPython/ranges.py
@@ -0,0 +1,12 @@
+#
+# Author: James Ortiz
+# File: ranges.py
+# Description: Ranges in python
+#
+
+print(range(10))
+print(list(range(10)))
+print(range(5, 10))
+print(list(range(5, 10)))
+print(list(range(5, 10, 2)))
+print(list(range(10, 1, -1)))
