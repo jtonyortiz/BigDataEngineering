71404128ad68b18b535784af046af11ad28cb747
James Ortiz <jamesaortiz75@gmail.com>
2021-12-25 23:46:24 -0500

    Strings in Python

---
+++ PythonDS/BasicPython/strings.py
@@ -0,0 +1,20 @@
+#
+# Author: James Ortiz
+# File: strings.py
+# Description: Uses of methods in strings
+#
+#
+
+my_name = "David"
+print(my_name)
+print(my_name[3])
+print(my_name * 2)
+print(len(my_name))
+
+
+#Useing String Methods in python
+print('Printing a string to upper case: ', my_name.upper())
+print('Centering a string in python: ', my_name.center(10))
+print('Finding v in the string David: ', my_name.find('v'))
+print('Splitting David with the delimiter at v: ', my_name.split('v'))
+
