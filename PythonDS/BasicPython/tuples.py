fe14dcb96ec7ac1a9446ad917bd5cd12b395f2e5
James Ortiz <jamesaortiz75@gmail.com>
2021-12-26 13:23:56 -0500

    Modified tuples.py

--- PythonDS/BasicPython/tuples.py
+++ PythonDS/BasicPython/tuples.py
@@ -8,3 +8,5 @@
 my_tuple = (2, True, 4.96)
 print(my_tuple)
 print('Length of the tuple: ', len(my_tuple))
+print('Printing the first element of the tuple: ', my_tuple[0])
+print('Printing the first two elements of the tuple: ', my_tuple[0:2])
